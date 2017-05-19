052cc0cb4659e435b7d2f99e23861d57a6893d15
Travis Overby <toverby@on-site.com>
2017-05-19 09:37:04 -0700

    Alter Emmet-Vim configuration, update init file

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@
 cp .vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+cp html.vim ~/.vim/bundle/emmet-vim/autoload/emmet/lang/html.vim
+rm html.vim
